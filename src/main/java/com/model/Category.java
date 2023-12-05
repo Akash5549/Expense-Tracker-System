b9005b3a1c54c75275f0c11b88feb60726cc939a
Akash5549 <akashjp.5549@gmail.com>
2023-12-05 21:51:53 +0530

    First Commit

---
+++ src/main/java/com/model/Category.java
@@ -0,0 +1,43 @@
+package com.model;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name="category")
+public class Category {
+	@Id
+	private Long id;
+
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
